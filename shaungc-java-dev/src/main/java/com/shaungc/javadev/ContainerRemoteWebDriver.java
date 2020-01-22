fa79c6eceaf34dce5ef79d33fb8f9ecd23a8f169
Shaung Cheng <shaungc@umich.edu>
2020-01-22 00:40:29 -0800

    add in headless config and add chromedriver path

--- shaungc-java-dev/src/main/java/com/shaungc/javadev/ContainerRemoteWebDriver.java
+++ shaungc-java-dev/src/main/java/com/shaungc/javadev/ContainerRemoteWebDriver.java
@@ -3,6 +3,7 @@ package com.shaungc.javadev;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -17,6 +18,16 @@ public class ContainerRemoteWebDriver extends RemoteWebDriver{
     }
     public ContainerRemoteWebDriver() throws MalformedURLException {
         // super(new URL("http://host.docker.internal:4444/wd/hub"), new ChromeOptions());
-        super(new URL("http://localhost:4444/wd/hub"), new ChromeOptions());
+        super(new URL("http://localhost:4444/wd/hub"), ContainerRemoteWebDriver.createDesiredCapabilities());
+    }
+    public static DesiredCapabilities createDesiredCapabilities() {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.setBinary("/tmp/chromedriver_linux");
+        chromeOptions.addArguments("--headless");
+        desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+
+        return desiredCapabilities;
     }
 }
