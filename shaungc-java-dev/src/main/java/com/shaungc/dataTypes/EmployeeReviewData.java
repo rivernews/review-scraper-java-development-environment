11a56cccfcb331d308c6f66bc0c8e734d97c5c4a
Shaung <shaungc@umich.edu>
2020-02-23 09:48:35 +0000

    add new to data classes

--- shaungc-java-dev/src/main/java/com/shaungc/dataTypes/EmployeeReviewData.java
+++ shaungc-java-dev/src/main/java/com/shaungc/dataTypes/EmployeeReviewData.java
@@ -6,8 +6,8 @@ import com.shaungc.utilities.Logger;
  * ReviewParsedData
  */
 public class EmployeeReviewData {
-    public StableReviewData stableReviewData;
-    public VaryingReviewData varyingReviewData;
+    public StableReviewData stableReviewData = new StableReviewData();
+    public VaryingReviewData varyingReviewData = new VaryingReviewData();
 
     public void debug(Integer messageNumber) {
         final Integer baseOneIndexMessageNumber = messageNumber + 1;
